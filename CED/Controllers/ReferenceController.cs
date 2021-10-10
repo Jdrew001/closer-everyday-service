1c02906c45e15dc4fdffe358313ad138bc2a6682
Drew Atkison <dtatkison@gmail.com>
2021-10-10 07:09:19 -0500

    reference controller added

---
+++ CED/Controllers/ReferenceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CED.Controllers
+{
+    [Authorize]
+    [Route("api/reference")]
+    [ApiController]
+    public class ReferenceController : CEDBaseController
+    {
+        public ReferenceController()
+        {
+
+        }
+
+        [HttpGet("getHabitReferenceData")]
+        public async Task<IActionResult> GetAllHabitReferenceData()
+        {
+            // TODO: Call service
+            return Ok();
+        }
+    }
+}
