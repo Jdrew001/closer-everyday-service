aad5bc50876a6575c8948ac6c736e2fafe1497f3
Drew Atkison <dtatkison@gmail.com>
2021-10-25 14:50:47 -0500

    sanity check added

--- CED/Controllers/AccountController.cs
+++ CED/Controllers/AccountController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CED.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
