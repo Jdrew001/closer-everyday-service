2584eacbb005758b8c64f632d5a71b8f7f65c309
Drew Atkison <dtatkison@gmail.com>
2021-10-28 13:33:43 -0500

    Create/update schedule repo

---
+++ CED.Data/Interfaces/IScheduleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CED.Models.Core;
+
+namespace CED.Data.Interfaces
+{
+    public interface IScheduleRepository
+    {
+        Task<Schedule> SaveSchedule(Schedule schedule);
+        Task<Schedule> UpdateSchedule(Schedule schedule);
+    }
+}
---
+++ CED.Data/Repositories/ScheduleRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CED.Data.Interfaces;
+using CED.Models;
+using CED.Models.Core;
+using Microsoft.Extensions.Options;
+
+namespace CED.Data.Repositories
+{
+    public class ScheduleRepository : DataProvider, IScheduleRepository
+    {
+        private readonly IScheduleTypeRepository _scheduleTypeRepository;
+
+        public ScheduleRepository(
+            IOptions<ConnectionStrings> connectionStrings,
+            IScheduleTypeRepository scheduleTypeRepository)
+            : base(connectionStrings.Value.CEDDB)
+        {
+            _scheduleTypeRepository = scheduleTypeRepository;
+        }
+
+        public async Task<Schedule> SaveSchedule(Schedule schedule)
+        {
+            string spName = "SaveSchedule";
+            using DataConnectionProvider dcp = CreateConnection();
+            await using var command = dcp.CreateCommand(spName);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("ScheduleTypeId", schedule.ScheduleType.Id);
+            command.Parameters.AddWithValue("UserId", schedule.UserId);
+            command.Parameters.AddWithValue("ScheduleTime", schedule.ScheduleTime);
+
+            using DataReaderHelper drh = await command.ExecuteReaderAsync();
+            while (drh.Read())
+                schedule = ReadSchedule(drh);
+
+            return schedule;
+        }
+
+        public async Task<Schedule> UpdateSchedule(Schedule schedule)
+        {
+            string spName = "UpdateSchedule";
+            using DataConnectionProvider dcp = CreateConnection();
+            await using var command = dcp.CreateCommand(spName);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Id", schedule.Id);
+            command.Parameters.AddWithValue("ScheduleTypeId", schedule.ScheduleType.Id);
+            command.Parameters.AddWithValue("UserId", schedule.UserId);
+            command.Parameters.AddWithValue("ScheduleTime", schedule.ScheduleTime);
+
+            using DataReaderHelper drh = await command.ExecuteReaderAsync();
+            while (drh.Read())
+                schedule = ReadSchedule(drh);
+
+            return schedule;
+        }
+
+        private Schedule ReadSchedule(DataReaderHelper drh)
+        {
+            return new Schedule()
+            {
+                Id = drh.Get<int>("Id"),
+                ScheduleTime = drh.Get<DateTime>("ScheduleTime"),
+                ScheduleType = new ScheduleType()
+                {
+                    Id = drh.Get<int>("idschedule_type"),
+                    Value = drh.Get<string>("scheduleType")
+                },
+                UserId = drh.Get<int>("UserId")
+            };
+        }
+    }
+}
