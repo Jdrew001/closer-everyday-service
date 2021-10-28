7e5fb8699d081697a826c7e760cd68973a2bc2f3
Drew Atkison <dtatkison@gmail.com>
2021-10-28 15:29:50 -0500

    schedule service added

---
+++ CED.Services/Core/ScheduleService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CED.Data.Interfaces;
+using CED.Models.Core;
+using CED.Services.Interfaces;
+
+namespace CED.Services.Core
+{
+    public class ScheduleService : IScheduleService
+    {
+        private readonly IScheduleRepository _scheduleRepository;
+
+        public ScheduleService(
+            IScheduleRepository scheduleRepository)
+        {
+            _scheduleRepository = scheduleRepository;
+        }
+
+        public async Task<Schedule> GetScheduleByHabitId(int habitId)
+        {
+            return await _scheduleRepository.GetScheduleByHabitId(habitId);
+        }
+
+        public async Task<Schedule> SaveSchedule(Schedule schedule)
+        {
+            return await _scheduleRepository.SaveSchedule(schedule);
+        }
+
+        public async Task<Schedule> UpdateSchedule(Schedule schedule)
+        {
+            return await _scheduleRepository.UpdateSchedule(schedule);
+        }
+    }
+}
---
+++ CED.Services/Interfaces/IScheduleService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CED.Models.Core;
+
+namespace CED.Services.Interfaces
+{
+    public interface IScheduleService
+    {
+        public Task<Schedule> GetScheduleByHabitId(int habitId);
+        public Task<Schedule> SaveSchedule(Schedule schedule);
+        public Task<Schedule> UpdateSchedule(Schedule schedule);
+    }
+}
