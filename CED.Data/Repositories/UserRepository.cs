a6e5a5c9f853f25cac27fb565f7fbf6fce5b0b07
Drew Atkison <dtatkison@gmail.com>
2021-11-01 21:40:01 -0500

    Fixes to user repository and logs added

--- CED.Data/Repositories/UserRepository.cs
+++ CED.Data/Repositories/UserRepository.cs
@@ -9,15 +9,20 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace CED.Data.Repositories
 {
     public class UserRepository : DataProvider, IUserRepository
     {
+        private readonly ILogger<UserRepository> _log;
+
         public UserRepository(
+            ILogger<UserRepository> log,
             IOptions<ConnectionStrings> connectionStrings)
             : base(connectionStrings.Value.CEDDB)
         {
+            _log = log;
         }
 
         public async Task<User> GetUserById(Guid userId)
@@ -27,81 +32,119 @@ namespace CED.Data.Repositories
 
         public async Task CreateNewUser(RegistrationDTO registrationDTO)
         {
-            var hashAndSalt = Hash.GetHash(registrationDTO.password);
-            string spName = "RegisterAccount";
-            using DataConnectionProvider dcp = CreateConnection();
-            await using var command = dcp.CreateCommand(spName);
+            _log.LogDebug("UserRepository: Start CreateNewUser : {Registration DTO}", registrationDTO);
+            try
+            {
+                var hashAndSalt = Hash.GetHash(registrationDTO.password);
+                string spName = "RegisterAccount";
+                using DataConnectionProvider dcp = CreateConnection();
+                await using var command = dcp.CreateCommand(spName);
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("Firstname", registrationDTO.firstName);
-            command.Parameters.AddWithValue("Lastname", registrationDTO.lastName);
-            command.Parameters.AddWithValue("Email", registrationDTO.email);
-            command.Parameters.AddWithValue("UserHash", hashAndSalt.Hash);
-            command.Parameters.AddWithValue("Salt", hashAndSalt.Salt);
-            command.Parameters.AddWithValue("DeviceGUID", registrationDTO.deviceGuid);
-            command.Parameters.AddWithValue("DeviceModel", registrationDTO.deviceModel);
-            command.Parameters.AddWithValue("DevicePlatform", registrationDTO.devicePlatform);
-            command.Parameters.AddWithValue("Manufacturer", registrationDTO.deviceManufacture);
-            await command.ExecuteNonQueryAsync();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("Firstname", registrationDTO.firstName);
+                command.Parameters.AddWithValue("Lastname", registrationDTO.lastName);
+                command.Parameters.AddWithValue("Email", registrationDTO.email);
+                command.Parameters.AddWithValue("UserHash", hashAndSalt.Hash);
+                command.Parameters.AddWithValue("Salt", hashAndSalt.Salt);
+                command.Parameters.AddWithValue("DeviceGUID", registrationDTO.deviceGuid);
+                command.Parameters.AddWithValue("DeviceModel", registrationDTO.deviceModel);
+                command.Parameters.AddWithValue("DevicePlatform", registrationDTO.devicePlatform);
+                command.Parameters.AddWithValue("Manufacturer", registrationDTO.deviceManufacture);
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception e)
+            {
+                _log.LogCritical(e, "UserRepository ERROR: Exception occurred in (CreateNewUser) Registration : {registrationDTO}", registrationDTO);
+            }
         }
 
         public async Task<User> GetUserByEmail(string email)
         {
+            _log.LogDebug("UserRepository: Start Get User By Email : {Email}", email);
             User result = null;
-            using DataConnectionProvider dcp = CreateConnection();
-            await using var command = dcp.CreateCommand("GetUserByEmail");
+            try
+            {
+                using DataConnectionProvider dcp = CreateConnection();
+                await using var command = dcp.CreateCommand("GetUserByEmail");
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("Email", email);
-            using DataReaderHelper drh = await command.ExecuteReaderAsync();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("Email", email);
+                using DataReaderHelper drh = await command.ExecuteReaderAsync();
 
-            while (drh.Read())
-                result = ReadUser(drh);
+                while (drh.Read())
+                    result = ReadUser(drh);
+            }
+            catch (Exception e)
+            {
+                _log.LogCritical(e, "UserRepository ERROR: Exception occurred in (GetUserByEmail) Email : {email}", email);
+            }
 
             return result;
         }
 
         public async Task<User> GetUserByRefreshToken(RefreshTokenDTO refreshTokenDTO)
         {
+            _log.LogDebug("UserRepository: Start Get User By Refresh Token : {Refresh Token DTO}", refreshTokenDTO);
             User result = null;
-            using DataConnectionProvider dcp = CreateConnection();
-            await using var command = dcp.CreateCommand("GetUserByRefreshToken");
+            try
+            {
+                using DataConnectionProvider dcp = CreateConnection();
+                await using var command = dcp.CreateCommand("GetUserByRefreshToken");
 
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("Token", refreshTokenDTO.Token);
-            using DataReaderHelper drh = await command.ExecuteReaderAsync();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("Token", refreshTokenDTO.Token);
+                using DataReaderHelper drh = await command.ExecuteReaderAsync();
 
-            while (drh.Read())
-                result = ReadUser(drh);
-
+                while (drh.Read())
+                    result = ReadUser(drh);
+            }
+            catch (Exception e)
+            {
+                _log.LogCritical(e, "UserRepository ERROR: Exception occurred in (GetUserByRefreshToken) Refresh Token : {refreshTokenDTO}", refreshTokenDTO);
+            }
+            
             return result;
         }
 
         public async Task Logout(string token)
         {
-            string spName = "RevokeToken";
-            using DataConnectionProvider dcp = CreateConnection();
-            await using var command = dcp.CreateCommand(spName);
-            command.CommandType = CommandType.StoredProcedure;
-            command.Parameters.AddWithValue("token", token);
-            await command.ExecuteNonQueryAsync();
+            _log.LogDebug("UserRepository: Start Logout : {Token}", token);
+            try
+            {
+                string spName = "RevokeToken";
+                using DataConnectionProvider dcp = CreateConnection();
+                await using var command = dcp.CreateCommand(spName);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("token", token);
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception e)
+            {
+                _log.LogCritical(e, "UserRepository ERROR: Exception occurred in (Logout) Token : {token}", token);
+            }
         }
 
         private User ReadUser(DataReaderHelper drh)
         {
-            return new User()
+            Guid guid;
+            if (Guid.TryParse(drh.Get<string>("iduser"), out guid))
             {
-                Id = new Guid(drh.Get<string>("iduser")),
-                Email = drh.Get<string>("email"),
-                Username = drh.Get<string>("username"),
-                FirstName = drh.Get<string>("firstname"),
-                LastName = drh.Get<string>("lastname"),
-                LastLogin = drh.Get<DateTime?>("lastLogin"),
-                Locked = drh.Get<bool>("locked"),
-                DateLocked = drh.Get<DateTime?>("datelocked"),
-                PasswordSalt = drh.Get<string>("passwordSalt"),
-                Password = drh.Get<string>("password")
-            };
+                return new User()
+                {
+                    Id = guid,
+                    Email = drh.Get<string>("email"),
+                    Username = drh.Get<string>("username"),
+                    FirstName = drh.Get<string>("firstname"),
+                    LastName = drh.Get<string>("lastname"),
+                    LastLogin = drh.Get<DateTime?>("lastLogin"),
+                    Locked = drh.Get<bool>("locked"),
+                    DateLocked = drh.Get<DateTime?>("datelocked"),
+                    PasswordSalt = drh.Get<string>("passwordSalt"),
+                    Password = drh.Get<string>("password")
+                };
+            }
+
+            return null;
         }
     }
 }
