16449678856f71356f8e1c0aaa24b4a8f9bc61d5
Drew Atkison <dtatkison@gmail.com>
2022-05-03 19:44:35 -0500

    fixed email

--- CED/Controllers/AccountController.cs
+++ CED/Controllers/AccountController.cs
@@ -38,17 +38,20 @@ namespace CED.Controllers
         [HttpGet("email")]
         public async Task<IActionResult> sendTestEmailAsync()
         {
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("admin@atkisondevserver.me", "Example User");
-            var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("dtatkison@gmail.com", "Example User");
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+            var message = new SendGridMessage();
 
-            return Ok(response);
+            // message.SetTemplateId("d-d336182397714541874da8d70a480139");
+            // message.AddTo("dtatkison@gmail.com", "Drew Atkison");
+            // message.SetFrom("admin@atkisondevserver.me", "Closer Everyday Admin");
+            // message.SetSubject("Testing Email");
+            // //message.SetTemplateData({"test": "test"});
+
+
+            // var apiKey = "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";
+            // var client = new SendGridClient(apiKey);
+            // var response = await client.SendEmailAsync(message);
+
+            return Ok("");
         }
 
         [HttpGet("environment")]
