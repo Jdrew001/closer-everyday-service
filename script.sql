a983539cd398f6f62f3e501600b2e46c78fe0f4d
Drew Atkison <dtatkison@gmail.com>
2022-05-07 15:45:32 -0500

    fixed revoke token script

--- script.sql
+++ script.sql
@@ -1,2 +1,27 @@
-ALTER TABLE `ced_dev`.`blacklisted_token` 
-CHANGE COLUMN `token` `token` LONGTEXT NOT NULL ;
+-- Drop stored procedure if exists
+-- Revoke token
+DROP PROCEDURE IF EXISTS RevokeToken;
+
+DELIMITER //
+
+CREATE PROCEDURE RevokeToken(
+	IN 
+		appToken longtext,
+        appTokenExpiry datetime,
+        refreshToken VARCHAR(255)
+)
+BEGIN
+	SET @id = UUID();
+    
+	DELETE FROM `CEDDB`.`refresh_token` re
+	WHERE re.`token` = refreshToken;
+    
+    INSERT INTO `ceddb`.`blacklisted_token`
+	(`id`, `token`, `expiry`) 
+	VALUES (UUID(), appToken, appTokenExpiry);
+
+END //
+
+DELIMITER ;
+-- End RevokeToken
+-- --------------------------------------
