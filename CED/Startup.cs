e1b147ce56e75814a50b1cc8b90b171219af5801
Drew Atkison <dtatkison@gmail.com>
2022-05-11 11:32:03 -0500

    test fix2

--- CED/Startup.cs
+++ CED/Startup.cs
@@ -103,11 +103,6 @@ namespace CED
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
-
             // global cors policy
             app.UseCors(x => x
                 .AllowAnyMethod()
@@ -134,6 +129,11 @@ namespace CED
 
             app.UseSerilogRequestLogging();
             app.UseBlackListTokenMiddleware();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseEndpoints(endpoints =>
             {
