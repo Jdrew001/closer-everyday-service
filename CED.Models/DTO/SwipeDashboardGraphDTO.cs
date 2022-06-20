138b4da325f31dce83e2b320bf7b0a299f4a2294
Drew Atkison <dtatkison@gmail.com>
2022-06-20 11:37:02 -0500

    update to swipe strat

--- CED.Models/DTO/SwipeDashboardGraphDTO.cs
+++ CED.Models/DTO/SwipeDashboardGraphDTO.cs
@@ -1,13 +1,11 @@
 namespace CED.Models.DTO
 {
-  public class SwipeDashboardGraphDTO
-  {
-    public int Limit { get; set; }
+    public class SwipeDashboardGraphDTO
+    {
+        public int Limit { get; set; }
 
-    public string BoundaryStartDate { get; set; }
+        public string DateSelected { get; set; }
 
-    public string BoundaryEndDate { get; set; }
-
-    public string Boundary { get; set; }
-  }
+        public string Boundary { get; set; }
+    }
 }
--- CED.Services/Strategies/GraphStrategies/SwipeStrategy.cs
+++ CED.Services/Strategies/GraphStrategies/SwipeStrategy.cs
@@ -1,29 +1,38 @@
 using CED.Models.Core;
 using CED.Models.DTO;
 using CED.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 
 namespace CED.Services.Strategies.GraphStrategies
 {
-  public class SwipeStrategy : IDashboardGraphStrategy
-  {
-    public override List<GraphDataResponseDTO> createGraphData<T>(T data, List<HabitLog> logs)
+    public class SwipeStrategy : IDashboardGraphStrategy
     {
-      var swipeDTO = data as SwipeDashboardGraphDTO;
+        public override List<GraphDataResponseDTO> createGraphData<T>(T data, List<HabitLog> logs)
+        {
+            var swipeDTO = data as SwipeDashboardGraphDTO;
+            dateSelected = swipeDTO.DateSelected;
+            this.logs = logs;
 
-      return null;
+            var result = GetWeekBoundaries(dateSelected, swipeDTO.Limit);
+            var dto = result.ConvertAll(new Converter<Dictionary<WeekBoundary, string>, GraphDataResponseDTO>(ConvertToGraphDataDTO));
+            foreach (var item in dto)
+                item.Selected = false;
+
+            if (swipeDTO.Boundary.ToUpper() == "START")
+                dto[0].Selected = true;
+            else
+            {
+                dto.Reverse();
+                dto[0].Selected = true;
+            }
+
+            return dto;
+        }
+
+        public override int CalculateWeekMultiple(int index, int startingIndex, int middleIndex, int endingIndex)
+        {
+            return (index * -1) * weekDifference;
+        }
     }
-
-    public override int CalculateWeekMultiple(int index, int startingIndex, int middleIndex, int endingIndex)
-    {
-
-      /*
-        For a give date
-       
-       */
-
-
-      throw new System.NotImplementedException();
-    }
-  }
 }
